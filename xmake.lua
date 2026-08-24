129fc54ce8a9e08b423e28089128664943ad71dd
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-23 22:47:35 -0600

    feat: project update

--- xmake.lua
+++ xmake.lua
@@ -30,3 +30,6 @@ target("BakaSurvivalSettings")
 
     -- add extra files
     add_extrafiles(".clang-format")
+
+    -- add install files
+    add_installfiles("res/MCM/**.*", { prefixdir = "MCM/config/BakaSurvivalSettings" })
